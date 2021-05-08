88ef2719470f623300adc9715358da40deafae1d
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-05-08 11:33:25 +0200

    pokus o fungovanie hry

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-/* Ahoj, asi sa divíš, prečo tu mám len jednu stránku tejto hry- dovod je, že som si opravením stylovania hracej plochy rozjebkala cele css a nestihla som to opravit, pretože pardon ale tento js kod mi dal strasne zabrat, nez som to napisala tak aby to aj fungovalo... takze odpust, dufam ze do buducej ulohy to stihnem dat cele dokopy*/
 
 let kdoJeNaTahu = 'circle';
 
@@ -14,5 +13,95 @@ document.querySelectorAll('.policko').forEach((btn) => {
       document.querySelector('.ikona-hrac').src = 'cross.svg';
       kdoJeNaTahu = 'circle';
     }
+    if (isWinningMove(btn)) {
+      setTimeout(() => {
+        if (kdoJenaTahu === 'cross') {
+          confirm('Koliesko vyhrává! Spustiť novú hru?');
+          location.reload();
+        } else {
+          confirm('Křížik vyhrává! Spustiť novú hru?');
+          location.reload();
+        }
+      });
+    }
   });
 });
+const getSymbol = (field) => {
+  // Název třídy přizpůsob tvému kódu.
+  if (field.classList.contains('board__field--cross')) {
+    return 'circle';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'cross';
+  }
+};
+
+const boardSize = 10; // 10x10
+const fields = document.querySelectorAll('.policko'); // Selektor pozměň tak, aby odpovídal tvému kódu.
+
+const getField = (row, column) => fields[row * boardSize + column];
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1; // Jednička pro právě vybrané políčko
+  // Koukni doleva
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  // Koukni doprava
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  // Koukni nahoru
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  // Koukni dolu
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
